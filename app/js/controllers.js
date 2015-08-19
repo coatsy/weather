c90f00450d9549a6e9be6faa06df9f407474fb9c
Andrew Coates <acoat@microsoft.com>
2015-08-19 15:34:03 +1000

    added direction arrow filter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@ var weatherApp = angular.module('weatherApp', ['weatherFilters']);
 
 weatherApp.controller('WeatherCtrl', ['$scope', '$http', function ($scope, $http) {
 	
-	$scope.city = 'Sydney';
+	$scope.city = 'Canberra';
 	
 	$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.city).success(function(data) {
 	$scope.weather = data;
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,21 +1,41 @@
-angular.module('weatherFilters', []).filter('direction', function() {
+angular.module('weatherFilters', [])
+	.filter('direction', function() {
 	return function(input) {
-		return input < 11.25 ? 'N' :
-				input < 33.75 ? 'NNE' :	
-				input < 56.25 ? 'NE' :	
-				input < 78.75 ? 'ENE' :	
-				input < 101.25 ? 'E' :	
-				input < 123.75 ? 'ESE' :	
-				input < 146.25 ? 'SE' :	
-				input < 168.75 ? 'SSE' :	
-				input < 191.25 ? 'S' :	
-				input < 213.75 ? 'SSW' :	
-				input < 236.25 ? 'SW' :	
-				input < 258.75 ? 'WSW' :	
-				input < 281.25 ? 'W' :	
-				input < 303.75 ? 'WNW' :	
-				input < 326.25 ? 'NW' :	
-				input < 348.75 ? 'NNW' :	
-				'N';
+		var answer =  
+			input < 11.25 ? 'N' :
+			input < 33.75 ? 'NNE' :	
+			input < 56.25 ? 'NE' :	
+			input < 78.75 ? 'ENE' :	
+			input < 101.25 ? 'E' :	
+			input < 123.75 ? 'ESE' :	
+			input < 146.25 ? 'SE' :	
+			input < 168.75 ? 'SSE' :	
+			input < 191.25 ? 'S' :	
+			input < 213.75 ? 'SSW' :	
+			input < 236.25 ? 'SW' :	
+			input < 258.75 ? 'WSW' :	
+			input < 281.25 ? 'W' :	
+			input < 303.75 ? 'WNW' :	
+			input < 326.25 ? 'NW' :	
+			input < 348.75 ? 'NNW' :	
+			'N';
+		return answer;
+		};
+	})
+	.filter('directionArrow', function() {
+	return function(input) {
+		console.log('direction arrow for ' + input);
+		var answer =  
+			input < 22.5 ? '\u2193' :
+			input < 67.5 ? '\u2199' :
+			input < 112.5 ? '\u2190' :
+			input < 157.5 ? '\u2196' :
+			input < 202.5 ? '\u2191' :
+			input < 247.5 ? '\u2197' :
+			input < 292.5 ? '\u2192' :
+			input < 337.5 ? '\u2198' :
+			'\u2193';	
+		console.log(answer);
+		return answer;
 	};
 });
